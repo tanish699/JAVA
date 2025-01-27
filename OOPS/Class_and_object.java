f4fe80b3643bc25f3109a10d31e87415a83fcfe9
tanish699 <tanishwadhwa2310@gmail.com>
2025-01-27 12:29:20 +0530

    Started OOPS

---
+++ OOPS/Class_and_object.java
@@ -0,0 +1,47 @@
+//Create class and objects
+
+public class Class_and_object {
+    public static void main(String[] args) {
+        
+        car car1 = new car();
+        car1.brand = "kia";
+        car1.Color = "black";
+        car1.speed = 120;
+        car1.print();
+
+        car car2 = new car("Skoda", "white", 0);
+        car2.print();
+
+        car car3 = new car("Sokda", null, 200);
+        car3.print();
+
+
+    }
+    
+}
+
+
+class car {
+    String Color;
+    String brand;
+    int speed;
+
+    void print(){
+        System.out.println("Brand : "+ brand);
+        System.out.println("Color : "+ Color);
+        System.out.println("Speed : "+ speed);
+    }
+
+    car(){  //override default costructor
+        brand = "";
+        Color = "White";
+        speed = 60;
+    }
+
+    car(String b, String c, int s){
+        Color = c;
+        speed = s;
+        brand = b;
+    }
+    
+}
---
+++ OOPS/Lec1.java
@@ -0,0 +1,28 @@
+public class Lec1 {
+    /*
+     * ->Classes
+     * 
+     * 
+     * Key Features of oops
+     * 1. Inheritance -> handling unneccesary functionality, Showing only key functionality
+     * 2. Polymorphism -> like a capsule, data members and methods are like a UNIT
+     * 3. Abstraction
+     * 4. Encepsulation
+     * 
+     * 
+     * 
+     * Class and object creation
+      Public class classname{
+      ---------
+      ----------
+      }
+
+
+      Constructors 
+      default -> auto created by memory
+
+      parametrized -> orguments input
+
+      Non-parameterized -> non-orguments input
+     */
+}
