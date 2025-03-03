d59a6333d47d69b627c897ff0c65210b9f2891b5
tanish699 <tanishwadhwa2310@gmail.com>
2025-03-03 12:49:06 +0530

    ArrayList

---
+++ Java Collection FrameWork/Array_List.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.*;
+
+public class Array_List {
+     public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        List<Integer> arr = new ArrayList<>();
+
+        //add element to list 
+        arrayList.add(10);
+        arrayList.add(20);
+
+        arrayList.add(1, 30);
+        System.out.println(arrayList);
+
+        // arrayList.add(5, 99); //Out of index
+        
+
+        System.out.println(arrayList.get(2));
+        System.out.println(arrayList.size());
+        arrayList.set(0, 69);
+        System.out.println(arrayList);
+        Collections.sort(arrayList);
+        System.out.println(arrayList);
+        arrayList.remove(2);
+        System.out.println(arrayList.contains(69));
+        System.out.println(arrayList);
+     }    
+}
---
+++ Java Collection FrameWork/tempCodeRunnerFile.java
@@ -0,0 +1,1 @@
+System.out.println(arrayList.size());
