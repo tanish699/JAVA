b54306e1b5abd2348fd26fe9f97c8222ce5e64bf
tanish699 <tanishwadhwa2310@gmail.com>
2025-04-08 11:37:36 +0530

    Started HashMaps

--- Codes/Chad.java
+++ Codes/Chad.java
@@ -6,7 +6,7 @@ public class Chad {
 
     public static void main(String[] args) {
 
-        String name = JOptionPane.showInputDialog("Konse Rang ki Chaddi pehne ho!?");
+        String name = JOptionPane.showInputDialog("Enter your name");
         JOptionPane.showMessageDialog(null, "Hello" + name);
 
     }
---
+++ Codes/MAin.java
@@ -0,0 +1,22 @@
+
+abstract class A{
+    // private abstract void display(){
+    //    System.out.println("g");
+    // }
+}
+
+// class B extends A{
+//     void display(){
+//         System.out.println("g");
+//     }
+// }
+
+
+
+public class MAin {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
+
+
